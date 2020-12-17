00245623c014d47efef2f85979e78b32df368b62
steve <steve.lowrance@gmail.com>
2020-12-16 23:46:57 -0800

    day16 part 1 complete

--- day16/day16_part2.py
+++ day16/day16_part2.py
@@ -1,4 +1,126 @@
-with open('input.txt') as f:
-    real_input = f.readlines()
+import copy
+from collections import defaultdict
 
-test_input = ''''''.splitlines()
+with open('input.txt') as f:
+    raw = f.read()
+    real_input = raw.split('\n\n')
+
+test_input = '''class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9'''.split('\n\n')
+
+
+used_input = real_input
+
+fields, my_ticket, near_tickets = used_input
+
+def get_field_values(fields: str):
+    values = set()
+    field_rules = defaultdict(set)
+    fields = fields.split('\n')
+    for field in fields:
+        field_name, rules = field.split(': ')
+        numbers = rules.split(' or ')
+        for num in numbers:
+            num1, num2 = num.split('-')
+            nums = range(int(num1), int(num2) + 1)
+            for num in nums:
+                values.add(num)
+                field_rules[field_name].add(num)
+    return values, field_rules
+
+def get_valid_tickets(near_tickets: str, valid_values: set) -> list:
+    valid_tickets = []
+    tickets = near_tickets.split('\n')
+    for ticket in tickets[1:]:
+        nums = ticket.split(',')
+        if all([int(num) in valid_values for num in nums]):
+            valid_tickets.append([int(num) for num in nums])
+    return valid_tickets
+
+fields_nums, field_rules = get_field_values(fields)
+valid_tickets = get_valid_tickets(near_tickets, fields_nums)
+print(valid_tickets)
+
+def possible_fields(valid_tickets):
+    all_possibles = []
+    for ticket in valid_tickets:
+        possibles = defaultdict(set)
+        for i, num in enumerate(ticket):
+            for k, v in field_rules.items():
+                if num in v:
+                    possibles[i].add(k)
+        all_possibles.append(possibles)
+    return all_possibles
+
+all_possibles = possible_fields(valid_tickets)
+field_list = {}
+field_names = field_rules.keys()
+for i in range(len(valid_tickets)):
+    field_list[i] = set(field_names)
+for possible in all_possibles:
+    for k, v in possible.items():
+        field_list[k] = field_list[k].intersection(possible[k])
+print(field_list)
+
+available_fields = set(field_names)
+print(available_fields)
+
+field_labels = {i: '' for i in range(len(field_names))}
+
+def get_field_labels(available_fields, field_list):
+    if len(field_list) == 0:
+        return None
+    if len(field_list) == 1:
+        return {}
+
+    for k, v in available_fields.items():
+        # if len(available_fields[k]) == 1 and v.pop() in field_labels:
+        #     continue
+        new_available_fields = copy.deepcopy(available_fields)
+        # v = v.intersection(field_list)
+        for i in v:
+
+            # new_field_list = v.intersection(field_list)
+            s = set()
+            s.add(i)
+            # available_fields[k] = s
+            new_field_list = copy.deepcopy(field_list)
+            # new_field_list.discard(i)
+            new_available_fields[k] = s
+            result = get_field_labels(new_available_fields, new_field_list)
+
+            if result is not None:
+                field_labels[k] = i
+                field_list.discard(i)
+                return field_labels
+
+        # return available_fields
+    return field_labels
+
+# def get_field_labels(field_list, available_fields):
+#     new_field_list = copy.copy(field_list)
+#     if len(available_fields) == 0:
+#         return field_labels
+#     for k, v in field_list.items():
+#         if len(v) == 1:
+#             field_labels[k] = v.pop()
+#             available_fields.discard(v)
+#             del new_field_list[k]
+#         if len(v) > 1:
+#             new_field_list[k] = v.intersection(available_fields)
+#     if '' in field_labels.values():
+#         get_field_labels(new_field_list, available_fields)
+#
+#
+#     return field_labels
+
+print(get_field_labels(field_list, available_fields))
---
+++ day17/day17_part1.py
@@ -0,0 +1,101 @@
+import copy
+
+with open('input.txt') as f:
+    real_input = [line.strip() for line in f.readlines()]
+
+test_input = '''.#.
+..#
+###'''.splitlines()
+
+used_input = real_input
+
+# for i in used_input:
+#     for j in i:
+
+initial = [[[j for j in i] for i in used_input]]
+
+def empty_field(size: int):
+    return [['.' for j in range(size)] for i in range(size)]
+
+def grow_matrix(old_matrix, size):
+    new_matrix = copy.deepcopy(old_matrix)
+    current_size = len(new_matrix[0])
+    for z in new_matrix:
+        if size > current_size:
+            for y in z:
+                y.insert(0, '.')
+                y.append('.')
+            z.insert(0, list('.' * size))
+            z.append(list('.' * size))
+    if size >= current_size:
+        new_matrix.insert(0, empty_field(size))
+        new_matrix.append(empty_field(size))
+    return new_matrix
+
+def count_active_neighbors(matrix, position):
+    active = 0
+    planes = len(matrix)
+    rows = len(matrix[0])
+    cols = len(matrix[0][0])
+    for z in [-1, 0, 1]:
+        for y in [-1, 0, 1]:
+            for x in [-1, 0, 1]:
+                if x == y == z == 0:
+                    continue
+                # is_seat = False
+                if position[0] + z >= planes or position[1] + y >= rows or position[2] + x >= cols:
+                    break
+                # if layout[seat[0] + r * depth][seat[1] + c * depth] in ['L', '#']:
+                #     is_seat = True
+                #     break
+
+                if 0 <= position[0] + z < planes and 0 <= position[1] + y < rows and 0 <= position[2] + x < cols:
+                    if matrix[position[0] + z][position[1] + y][position[2] + x] == '#':
+                        active += 1
+    return active
+
+def update_matrix(matrix):
+    new_matrix = copy.deepcopy(matrix)
+    for z, plane in enumerate(matrix):
+        for y, row in enumerate(plane):
+            for x, col in enumerate(row):
+                if col == '#' and count_active_neighbors(matrix, [z, y, x]) not in [2, 3]:
+                    new_matrix[z][y][x] = '.'
+                elif col == '.' and count_active_neighbors(matrix, [z, y, x]) == 3:
+                    new_matrix[z][y][x] = '#'
+    return new_matrix
+
+for z in initial:
+    for y in z:
+        print(y)
+# for z, plane in enumerate(initial):
+#     for y, row in enumerate(plane):
+#         for x, col in enumerate(row):
+#             print(count_active_neighbors(initial, [z,y,x]), col)
+
+size = len(used_input)
+matrix = copy.deepcopy(initial)
+for _ in range(6):
+    matrix = grow_matrix(matrix, size)
+    for z in matrix:
+        print('\n')
+        for y in z:
+            print(y)
+    matrix = update_matrix(matrix)
+    for z in matrix:
+        print('\n')
+        for y in z:
+            print(y)
+    size += 2
+for z in matrix:
+    print('\n')
+    for y in z:
+        print(y)
+
+count = 0
+for z, plane in enumerate(matrix):
+    for y, row in enumerate(plane):
+        for x, col in enumerate(row):
+            if col == '#':
+                count += 1
+print(count)
